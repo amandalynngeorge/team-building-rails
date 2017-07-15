da3eae3e70f76f1cb51fc40494d37d7bbcadb5f3
Amanda George <github email address>
2017-07-15 08:02:16 -0500

    add rest of activity controller methods

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -13,7 +13,28 @@ class ActivitiesController < ApplicationController
       render 'new'
     end
   end
-  
+
+  def edit
+  end
+
+  def update
+    if @activity.update(activity_params)
+      redirect_to activity_path(@activity)
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @activities = Activity.all
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
   private
     def find_activity
       @activity = Activity.find(params[:id])
