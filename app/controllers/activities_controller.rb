61eee24086d59b09309f0a0a23421529d2d995fb
Amanda George <github email address>
2017-07-15 07:59:55 -0500

    activity controller create

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,21 @@
 class ActivitiesController < ApplicationController
+  before_action :find_activity, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    if @activity.save
+      redirect_to activity_path(@activity)
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    def find_activity
+      @activity = Activity.find(params[:id])
+    end
 end
