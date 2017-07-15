c3d07ba97e456c4a9908de156f6b9d6b7203f4b8
Amanda George <github email address>
2017-07-15 08:18:49 -0500

    seed categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: "Ice Breaker")
+Category.create(name: "Active Team Initiative")
+Category.create(name: "Problem Solving Initiative")
+Category.create(name: "Trust Activity")
+Category.create(name: "Debrief")
