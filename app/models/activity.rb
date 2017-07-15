f058a955776c344292f2851be3bcd0cf65d126a1
Amanda George <github email address>
2017-07-15 07:31:05 -0500

    add associations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,5 @@
 class Activity < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :activities
+  has_many :categories, through: :activities
+
 end
