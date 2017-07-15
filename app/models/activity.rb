902f91f8ae63c2e3129049f1d80dbec1e4ed3020
Amanda George <github email address>
2017-07-15 08:22:55 -0500

    fix validation for activity title

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,7 +2,7 @@ class Activity < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  validates :title, length: {1..50}
+  validates :title, length: {in: 1..50}
   validates :description, presence: true
   validates :categories, length: {:minimum => 1}
 end
