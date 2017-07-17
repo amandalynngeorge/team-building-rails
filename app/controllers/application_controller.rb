7c8697d1fb75830435b34ec99b0b93d28a3eda23
Amanda George <github email address>
2017-07-17 12:34:10 -0500

    route to home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    session[:user_id] = @user.id
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
+  def require_login
+    unless current_user
+      flash[:error] = "Please log in"
+      redirect_to home_path
+    end
+  end
+
+  helper_method :logged_in?
+  helper_method :require_login
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   resources :activities
   resources :categories
   resources :users
+  resources :sessions
 
   get 'sign_up', to: 'user#new'
   get 'sign_in', to: 'session#new'
   post 'sign_in', to: 'session#create'
   get 'sign_out', to: 'session#destroy'
+
+  get '/', to: 'sessions#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
