fce7109870e2660df968a195114fb568de264cfa
Amanda George <github email address>
2017-07-17 14:46:28 -0500

    add links to welcome page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+
   def current_user
-    session[:user_id] = @user.id
+    if session[:user_id].present?
+      user = session[:user_id]
+    end
   end
 
   def logged_in?
-    current_user != nil
-  end
-
-  def require_login
-    unless current_user
+    if !current_user
       flash[:error] = "Please log in"
-      redirect_to home_path
+      redirect_to 'welcome#home'
     end
   end
+
 
   helper_method :logged_in?
   helper_method :require_login
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,37 @@ class SessionsController < ApplicationController
   def home
   end
 
+  def new
+    if current_user
+      redirect_to home_path
+    end
+  end
+
+  def create
+    if params[:user_id].present? && params[:user_id] != ""
+      sessions[:user_id] = params[:user_id]
+      redirect_to user_path
+    else
+      redirect_to home_path
+    end
+  end
+
+  def show
+    @user = current_user.username
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def destroy
+    if session[:user_id].present?
+      session.delete
+    else
+      redirect_to home_path
+    end
+  end
+
 end
