e7ce185dec377b546fc46024193d1175144aaf83
Amanda George <github email address>
2017-07-20 14:07:08 -0500

    create topics controller actions

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,35 @@
 class TopicsController < ApplicationController
+  before_action :find_topic, only: [:show, :edit, :update, :destroy]
+  # before_action :require_login
+
+  def new
+  end
+
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to topic_path(@topic)
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+    def find_topic
+      @topic = Topic.find_by(name: params[:name])
+    end
+
+    def topic_params
+      params.require(:topic).permit(:name)
+    end
+
+
 end
