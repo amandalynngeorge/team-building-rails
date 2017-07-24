fd6eb13289d1203cfd2ae9ab9f7ecd09e34af934
Amanda George <github email address>
2017-07-24 10:56:23 -0500

    update routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: [:show, :edit, :update, :destroy]
-  before_action :require_login, only: [:show]
-
+  before_action :require_login, only: [:show, :index]
 
   def new
     @user = User.new
@@ -10,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
       render 'new'
@@ -17,13 +16,16 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find_by(id: params[:id])
   end
 
+  def index
+    @user = User.find_by(id: params[:id])
+  end
 
 private
 
   def find_user
-    @user = User.find(params[:id])
   end
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,6 @@ Rails.application.routes.draw do
   resources :categories
   resources :users
 
-  get '/sign_up', to: 'users#new'
-  get '/sign_in', to: 'sessions#new'
-  post '/sign_in', to: 'sessions#create'
-  post '/sign_out', to: 'sessions#destroy'
-
   root 'sessions#home', as: 'home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
