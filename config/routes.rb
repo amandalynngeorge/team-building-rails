dc510f9cad5484a078ced2408134a7b7d36b141f
Amanda George <github email address>
2017-09-18 17:21:40 -0500

    add user/:user_id/acitivies routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :topics, only: [:new, :create, :index, :show]
-  resources :users, only: [:new, :create, :show, :index]
+  resources :users, only: [:new, :create, :show, :index] do
+    resources :activities
+  end
+
   resources :sessions
   resources :categories do
     resources :activities
@@ -16,4 +19,6 @@ Rails.application.routes.draw do
   get '/sign_out', to: "sessions#destroy"
   root 'sessions#home', as: 'home'
   get '/auth/facebook/callback' => 'sessions#create'
+
+  get '/users/:id/body', to: 'activities#body'
 end
