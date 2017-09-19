e907b5990e373c265b59b5b7d98df3173f30f25a
Amanda George <github email address>
2017-09-19 15:50:51 -0500

    add serializers

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -1,10 +1,10 @@
 $(function () {
   $(".js-next").on("click", function() {
     var nextId = parseInt($(".js-next").attr("data-id")) + 1;
-    $.getJSON("/posts/" + nextId + "/post_data", function(data) {
-      $(".authorName").text(data["author"]["name"]);
-      $(".postTitle").text(data["title"]);
-      $(".postBody").text(data["description"]);
+    $.getJSON("/activities/" + nextId + "/activity_data", function(data) {
+      $(".title").text(data["title"]);
+      $(".description").text(data["description"]);
+      // $(".postBody").text(data["description"]);
       // re-set the id to current on the link
       $(".js-next").attr("data-id", data["id"]);
     });
--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,4 +1,5 @@
 class ActivitySerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :goal, :rules, :time
   belongs_to :category, :user
+  has_many :topics
 end
