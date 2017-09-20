0203320b4a61535088d3fcc72c1bb4790167aa68
Amanda George <github email address>
2017-09-19 21:21:18 -0500

    clean up activity show page and show js

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -1,13 +1,13 @@
-$("#title").text(data.title);
-$(".description").text(data.description);
-
 $(function () {
   $(".js-next").on("click", function() {
     var nextId = parseInt($(".js-next").attr("data-id")) + 1;
-    $.getJSON("/activities/" + nextId + "/activity_data", function(data) {
+    $.get("/activities/" + nextId + "/activity_data", function(data) {
       $(".title").text(data["title"]);
       $(".description").text(data["description"]);
-      // $(".postBody").text(data["description"]);
+      $(".goal").text(data["goal"]);
+      $(".rules").text(data["rules"]);
+      $(".time").text(data["time"]);
+      $(".category").text(data["category"]["name"]);
       // re-set the id to current on the link
       $(".js-next").attr("data-id", data["id"]);
     });
