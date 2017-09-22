e8f5e246bd8764256cfe14d67c8530cf446fe384
Amanda George <github email address>
2017-09-22 07:57:33 -0500

    delete clutter work on form submit

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -21,27 +21,36 @@ $(document).ready(function() {
       $(".category").text(data["category"]["name"]);
     });
   })
+
+    // $(".submit").on("click", function(event) {
+    //   event.preventDefault()
+    //   $.ajax({
+    //     url: '/activities',
+    //     method: 'POST',
+    //     data: $(this).serialize(),
+    //     success: function(data) {
+    //       $('#acitivity_list').append(`<li>${data.content}</li>`)
+    //       console.log(data)
+    //     }
+    //   })
+    // })
+
+  //
+  // $('.submit').on("click", function(event) {
+  //   event.preventDefault()
+  //   var activity = $("form").serialize()
+  //   var activities = $.post("/activities", values)
+  //   //on submit:
+  //     //create resource with JS model object
+  //     //render response:
+  //       //title added to list as link
+  //       //full details available when link clicked on
+  //   console.log(activity)
+  //   console.log(acitivities)
+  // })
+  //
+
 })
-
-
-//
-// const bindClickHandlers = () => {
-//   $(".username_activities").on("click", (e) => {
-//     e.preventDefault()
-//     var userId = parseInt($("#username").attr("data-id"));
-//     fetch(`/users/${userId}/activities.json`)
-//       .then(res => res.json())
-//       .then(data => {
-//         $("#activity_id").html("")
-//         activities.forEach((activity) => {
-//           let newActivity = new Activity(activity)
-//           let activityHtml = newActivity.formatIndex()
-//           $('#username_activities').append(activityHtml)
-//           console.log(newActivity)
-//         })
-//       })
-//   })
-// }
 
 function Activity(id, title, description, goal, rules, time, category_id, topics_attributes) {
   activity.id = id
@@ -60,35 +69,3 @@ Activity.prototype.formatIndex = function() {
   `
   return activityHtml
 }
-
-//
-
-//   $(".submit").on("click", function(event) {
-//     event.preventDefault()
-//     $.ajax({
-//       url: '/activities',
-//       method: 'POST',
-//       data: $(this).serialize(),
-//       success: function(data) {
-//         $('#acitivity_list').append(`<li>${data.content}</li>`)
-//       }
-//     })
-//     // var url = $(this).data["url"]
-//     // console.log(url)
-//     // $.get(url), function(response) {
-//     //   console.log(response)
-//   })
-//   })
-//     // $.post($(this).attr('action', $(this).serialize(), function(response) {
-//     //   var activityId = parseInt($(this).attr('data-id'))
-//     //   $.get("/users/" + userId + "/activities/" + activityId + ".json", function(data) {
-//     //     $(".title").text(data["title"]);
-//     //     $(".description").text(data["description"]);
-//     //     $(".goal").text(data["goal"]);
-//     //     $(".rules").text(data["rules"]);
-//     //     $(".time").text(data["time"]);
-//     //     $(".category").text(data["category"]["name"]);
-//     // }, 'json')
-//     // $('form :input').attr('value', '')
-//
-// //close doc.ready
