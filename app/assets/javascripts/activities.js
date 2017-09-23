430f7b385a481eb23ed573c0358f35f1ea2afc8d
Amanda George <github email address>
2017-09-22 22:01:26 -0500

    uncomment activity.js and take out showActivity

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -1,15 +1,15 @@
-// $(function () {
-//   $(".js-next").on("click", function() {
-//     var nextId = parseInt($(".js-next").attr("data-id")) + 1;
-//     $.get("/activities/" + nextId + ".json", function(data) {
-//       $(".title").text(data["title"]);
-//       $(".description").text(data["description"]);
-//       $(".goal").text(data["goal"]);
-//       $(".rules").text(data["rules"]);
-//       $(".time").text(data["time"]);
-//       $(".category").text(data["category"]["name"]);
-//       // re-set the id to current on the link
-//       $(".js-next").attr("data-id", data["id"]);
-//     });
-//   });
-// });
+$(function () {
+  $(".js-next").on("click", function() {
+    var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+    $.get("/activities/" + nextId + ".json", function(data) {
+      $(".title").text(data["title"]);
+      $(".description").text(data["description"]);
+      $(".goal").text(data["goal"]);
+      $(".rules").text(data["rules"]);
+      $(".time").text(data["time"]);
+      $(".category").text(data["category"]["name"]);
+      // re-set the id to current on the link
+      $(".js-next").attr("data-id", data["id"]);
+    });
+  });
+});
--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -29,10 +29,10 @@ $(document).ready(function() {
       newActivity = new Activity(data.id, data.title, data.description, data.goal, data.rules, data.time, data.category_id, data.topics_attributes )
       var activityHtml = newActivity.formatShowLink()
       $('#activity_list').append(activityHtml)
-      $('.showActivity').html("")
-      var activityShowHtml = newActivity.formatShow()
-      $('.showActivity').html(activityShowHtml)
-      $('.showActivity').show()
+      // $('.showActivity').html("")
+      // var activityShowHtml = newActivity.formatShow()
+      // $('.showActivity').html(activityShowHtml)
+      // $('.showActivity').show()
     }).fail(function(data){
       debugger
     })
