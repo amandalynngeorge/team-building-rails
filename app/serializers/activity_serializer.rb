95b8a4fbe94e167296f89cba3750d981e3949c3e
Amanda George <github email address>
2017-09-23 07:49:08 -0500

    clean up activity serializer :

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -3,22 +3,4 @@ class ActivitySerializer < ActiveModel::Serializer
   belongs_to :category
   belongs_to :user
   has_many :topics
-
-  # def self.serialize(activity)
-  #
-  #   # start with the open brace to create a valid JSON object
-  #   serialized_activity = '{'
-  #
-  #   serialized_activity += '"id": ' + activity.id.to_s + ', '
-  #   serialized_activity += '"title": "' + activity.title + '", '
-  #   serialized_activity += '"description": "' + activity.description + '", '
-  #   serialized_activity += '"goal": "' + activity.goal + '", '
-  #   serialized_activity += '"rules": "' + activity.rules + '", '
-  #   serialized_activity += '"time": "' + activity.time.to_s + '", '
-  #   serialized_activity += '"category": {'
-  #   serialized_activity += '"name": "' + activity.category.name + '"}'
-  #
-  #   # and end with the close brace
-  #   serialized_activity += '}'
-  # end
 end
