d7d5d86c2c242941a71e2178a51461925986776f
Amanda George <github email address>
2017-09-25 10:59:43 -0500

    fixing user new activity form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,8 +21,6 @@ class UsersController < ApplicationController
   # 2. Sorting the activities by time_needed in your JS
   def show
     @user = User.find_by(id: params[:id])
-    # @new_activity = Activity.new
-    # @new_activity.topics.build
     if @user.activities.first
       @activity = @user.activities.first
     else
